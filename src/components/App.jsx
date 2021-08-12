dd3a1dba7522412cae157dae7a2a18132e0f5e39
Mxx1029 <lia.pim@icloud.com>
2021-08-12 16:35:18 +0200

    Finish bonus task distance travelled

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,151 +5,133 @@ const initialState = {
     gear: 0,
     speed: 0,
     engineStarted: false,
-    engineStopped: true,
     distanceTravelled: 0
 }
 
 function reducer(previousState, action) {
     switch (action.type) {
         case "start":
-            if (previousState.engineStarted === false && previousState.engineStopped === true) {
+            if (previousState.engineStarted === false) {
                 const startTry = Math.random();
                 if (startTry > 0.5) {
                     console.log(startTry)
                     return {
+                        ...previousState,
                         engineStarted: true,
-                        engineStopped: false,
-                        gear: 0,
-                        speed: previousState.speed,
-                        distanceTravelled: 0
                     }
-                } else if (startTry <= 0.5) {
-                    console.log(startTry)
-                    return {
-                        engineStarted: false,
-                        engineStopped: true,
-                        gear: 0,
-                        speed: previousState.speed,
-                        distanceTravelled: previousState.distanceTravelled
-                    }
-                }
+                } 
             }
             return previousState;
         case "stop":
-            if (previousState.engineStarted === true && previousState.engineStopped === false) {
+            if (previousState.engineStarted === true) {
                 return {
-                    engineStarted: false,
-                    engineStopped: true,
+                    ...previousState,
                     gear: 0,
-                    speed: previousState.speed,
-                    distanceTravelled: 0
+                    engineStarted: false,
                 }
             }
             return previousState;
         case "gearup":
-            if (previousState.engineStarted === true && previousState.engineStopped === false && previousState.gear < 5) {
+            if (previousState.engineStarted === true && previousState.gear < 5) {
                 return {
-                    engineStarted: true,
-                    engineStopped: false,
+                    ...previousState,
                     gear: previousState.gear + 1,
-                    speed: previousState.speed,
-                    distanceTravelled: previousState.distanceTravelled
                 }
             } 
             return previousState;
         case "geardown":
-            if (previousState.engineStarted === true && previousState.engineStopped === false && previousState.gear > -2) {
+            if (previousState.engineStarted === true && previousState.gear > -2) {
                 return {
-                    engineStarted: true,
-                    engineStopped: false,
+                    ...previousState,
                     gear: previousState.gear - 1,
-                    speed: previousState.speed
                 }
             } 
             return previousState;
         case "speedup":
-            if (previousState.engineStarted === true && previousState.engineStopped === false && previousState.gear !== 0 && previousState.speed < 225) {
-                switch (previousState.gear) {
-                    case -2:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 215 ? previousState.speed + 10 : 225
-                        }
-                    case -1:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 220 ? previousState.speed + 5 : 225
-                        }
-                    case 1:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 220 ? previousState.speed + 5 : 225,
-                            // distanceTravelled: setInterval(() => { previousState.distanceTravelled + 1.4;
-                            // }, 1000)
-                        }
-                    case 2:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 215 ? previousState.speed + 10 : 225
-                        }
-                    case 3:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 210 ? previousState.speed + 15 : 225
-                        }
-                    case 4:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 205 ? previousState.speed + 20 : 225
-                        }
-                    case 5:
-                        return {
-                            engineStarted: true,
-                            engineStopped: false,
-                            gear: previousState.gear,
-                            speed: previousState.speed <= 200 ? previousState.speed + 25 : 225
-                        }
-                    default:
-                        alert("Unknown action!!!")
-                        return previousState;
+            if (previousState.engineStarted === true && previousState.gear !== 0) {
+                return {
+                    ...previousState, 
+                    // Joels much shorter version to my endless switch statement: 
+                    speed: (previousState.speed + (5 * previousState.gear)) <= 225 ? previousState.speed + (5 * previousState.gear) : 225
                 }
+                // switch (previousState.gear) {
+                //     case -2:
+                //         return {
+                //             // engineStarted: true, // or: previousState.engineStarted
+                //             // gear: previousState.gear,
+                //             // distanceTravelled: previousState.distanceTravelled
+                //             // instead of the above 4 lines, you can spread them: 
+                //             ...previousState,
+                //             speed: previousState.speed <= 215 ? previousState.speed - 10 : 225
+                //         }
+                //     case -1:
+                //         return {
+                //             ...previousState,
+                //             speed: previousState.speed <= 220 ? previousState.speed - 5 : 225
+                //         }
+                //     case 1:
+                //         return {
+                //             ...previousState,
+                //             speed: previousState.speed <= 220 ? previousState.speed + 5 : 225,
+                //             // distanceTravelled: setInterval(() => { previousState.distanceTravelled + 1.4;
+                //             // }, 1000)
+                //         }
+                //     case 2:
+                //         return {
+                //             ...previousState,
+                //             speed: previousState.speed <= 215 ? previousState.speed + 10 : 225
+                //         }
+                //     case 3:
+                //         return {
+                //             ...previousState,
+                //             speed: previousState.speed <= 210 ? previousState.speed + 15 : 225
+                //         }
+                //     case 4:
+                //         return {
+                //             ...previousState,
+                //             speed: previousState.speed <= 205 ? previousState.speed + 20 : 225
+                //         }
+                //     case 5:
+                //         return {
+                //             ...previousState,
+                //             speed: previousState.speed <= 200 ? previousState.speed + 25 : 225
+                //         }
+                //     default:
+                //         alert("Unknown action!!!")
+                //         return previousState;
+                // }
                 
             } 
             return previousState;
         case "speeddown":
-            if (previousState.engineStarted === true && previousState.engineStopped === false && previousState.gear !== 0) {
+            if (previousState.engineStarted === true && previousState.gear !== 0) {
                 return {
                     engineStarted: true,
-                    engineStopped: false,
                     gear: previousState.gear,
-                    speed: previousState.speed >= 10 ? previousState.speed - 10 : 0
+                    speed: previousState.speed >= 10 ? previousState.speed - 10 : 0,
+                    distanceTravelled: previousState.distanceTravelled
                 }
             } 
             return previousState;
         case "move":
-            if (previousState.engineStarted === true && previousState.engineStopped === false && previousState.gear !== 0 && previousState.speed > 0) {
+            if (previousState.engineStarted === true && previousState.gear !== 0 && previousState.speed > 0) {
                 return {
-                    // engineStarted: true,
-                    // engineStopped: false,
-                    // gear: previousState.gear,
-                    // speed: previousState.speed,
                     ...previousState,
-                    distanceTravelled: Number(previousState.distanceTravelled) + Number(previousState.speed)
+                    // divide speed by 3.6 to get from km to meter
+                    distanceTravelled: previousState.distanceTravelled + previousState.speed/3.6
+                    // helper to see anything, got NaN first:
+                    // distanceTravelled: `${previousState.distanceTravelled} + ${previousState.speed}`
+                }
+            } else if (previousState.engineStarted === false && previousState.speed > 0 && previousState.distanceTravelled > 0) {
+                return {
+                    ...previousState,
+                    // divide speed by 3.6 to get from km to meter
+                    distanceTravelled: previousState.distanceTravelled + previousState.speed/3.6
                 }
             }
             return previousState;
+        case "reset":
+            return initialState;
         default:
             alert("Unknown action!!!")
             return previousState;
@@ -165,24 +147,32 @@ function App() {
     // with console.log("State is ", state) you get the contents of your initialState object desiplayed just fine
 
     useEffect(() => {
-        setInterval(() => {
-            dispatch({ type: "move"
-            // , state.distanceTravelled 
-        })
-        }, 1000)
+        const distanceMeter = setInterval(() => {
+            dispatch({ type: "move"});
+        }, 1000) 
+        return () => {
+            clearInterval(distanceMeter);
+        };
     }, [])
 
     return (
         <div className="App">
             <h1>This is a boat game.</h1>
-            {/* <p>Your boat engine is running: {`${state.engineStarted}`}, or has the engine been stopped: {`${state.engineStopped}`}</p> */}
+            {/* <p>Your boat engine is running: {`${state.engineStarted}`}</p> */}
             <p>Boat engine running? 
                 <br />
                 {state.engineStarted ? "✅" : "❌"}
             </p>
             <p>Current gear: <span className="bold">{state.gear}</span></p>
             <p>Current speed: <span className="bold">{state.speed}</span> km/h</p>
-            <p>Distance travelled: <span className="bold">{state.distanceTravelled}</span> m</p>
+            <p>Distance travelled: <span className="bold">
+                    {state.distanceTravelled < 1000 
+                        ? 
+                        Number(state.distanceTravelled.toFixed(2)) : 
+                        Number((state.distanceTravelled/1000).toFixed(2))
+                    }
+                </span> {state.distanceTravelled < 1000 ? "m" : "km"}
+            </p>
         
             <button onClick={() => dispatch({type: "start"})}>
                 Start engine
@@ -196,12 +186,17 @@ function App() {
             <button onClick={() => dispatch({type: "geardown"})}>
                 1 gear down
             </button>
-            <button onClick={() => dispatch({type: "speedup"}, {type: "move"})}>
+            <button onClick={() => dispatch({type: "speedup"})}>
                 Go faster
             </button>
             <button onClick={() => dispatch({type: "speeddown"})}>
                 Go slower
             </button>
+            <div className="wrapper">
+                <button onClick={() => dispatch({type: "reset"})}>
+                    Reset
+                </button>
+            </div>
             
         </div>
         
